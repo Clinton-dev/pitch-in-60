3c81c0126f55f57ec6837f9f291b846f84944f5f
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-06 22:48:37 +0300

    Add index file to templates

--- pitch.py
+++ pitch.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+pitches = [
+    {
+        "user":"Clinton dev",
+        "description":"Its not you its me",
+        "time_created":"09:30 am"
+    },
+    {
+        "user":"Clinton dev",
+        "description":"If you are going through hell keep moving",
+        "time_created":"09:50 am"
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    title ="Pitch in a min"
+    return render_template("home.html", pitches=pitches, title=title)
 
 
 
