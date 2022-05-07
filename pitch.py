3fbdf44a633c88a5fc05e198ebc905c0e106c638
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-07 12:47:19 +0300

    Add signup and login routes

--- pitch.py
+++ pitch.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='df84a75c801a375f226ab9fd83b82a'
 
 pitches = [
     {
@@ -16,10 +19,19 @@ pitches = [
 ]
 
 @app.route("/")
-def hello_world():
+def index():
     title ="Pitch in a min"
     return render_template("home.html", pitches=pitches, title=title)
 
+@app.route("/signup")
+def signup():
+    form = LoginForm()
+    return render_template("signup.html", title="Signup", form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Signup", form=form)
 
 
 
