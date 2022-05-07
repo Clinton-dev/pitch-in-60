581b76bb093dcb16ac65b212468508bd581878cc
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-07 23:44:00 +0300

    Add flask-bcrypt extension

--- pitch/__init__.py
+++ pitch/__init__.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='df84a75c801a375f226ab9fd83b82a'
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from pitch import routes
