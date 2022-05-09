ec29cf13c2bb22b3e434d93730daf12546463783
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-09 20:36:49 +0300

    Move welcome email and save pic method to utils

---
+++ pitch/users/utils.py
@@ -0,0 +1,20 @@
+def send_welcome_email(user):
+    msg = Message('Welcome to pitch-in-60', sender="lastcam00@gmail.com", recipients=[user.email])
+    msg.body = f"""We have just seen you have signed up for our application and want to welcome you. Please enjoy the list of all your  favourite pitches. Please login at: {url_for('login', _external=True)}
+    """
+    mail.send(msg)
+
+
+
+#
+def save_picture(form_picture):
+    random_hex =  secrets.token_hex(7)
+    f_name, f_ext = os.path.splitext(form_picture.filename)
+    pic_file_name = random_hex + f_ext
+    pic_path = os.path.join(app.root_path, 'static/profile-pics', pic_file_name)
+
+    output_size = (127, 127)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(pic_path)
+    return pic_file_name
