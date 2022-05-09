77e5b90c2c80aadf646cc6d4727232603bc47c79
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-09 21:05:27 +0300

    Add correct imports to pitches routes

--- pitch/pitches/routes.py
+++ pitch/pitches/routes.py
@@ -1,4 +1,9 @@
-from flask import Blueprint
+from flask import render_template, url_for, flash, redirect, request, abort,Blueprint
+from flask_login import current_user, login_required
+from pitch import db
+from pitch.models import Pitch
+from pitch.pitches.forms import PitchForm
+
 
 pitches = Blueprint('pitches', __name__)
 
