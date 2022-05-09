1046d0074cf9f76855e2c3354f6a3e20ff3050e9
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-09 20:47:45 +0300

    Move routes to main package

---
+++ pitch/main/routes.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+main = Blueprint('main', __name__)
+
+@main.route("/")
+def index():
+    page = request.args.get('page', 1, type=int)
+    pitches = Pitch.query.order_by(Pitch.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template("home.html", pitches=pitches, title="Pitch in a min")
