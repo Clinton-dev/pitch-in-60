6b818a90f695d201eef5ff87ee79a7bba1dc9a1c
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-09 21:14:33 +0300

    Add correct import to user utils

--- pitch/users/utils.py
+++ pitch/users/utils.py
@@ -1,3 +1,10 @@
+import os
+import secrets
+from PIL import Image
+from flask import url_for
+from flask_mail import Message
+from pitch import app, mail
+
 def send_welcome_email(user):
     msg = Message('Welcome to pitch-in-60', sender="lastcam00@gmail.com", recipients=[user.email])
     msg.body = f"""We have just seen you have signed up for our application and want to welcome you. Please enjoy the list of all your  favourite pitches. Please login at: {url_for('login', _external=True)}
