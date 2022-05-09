8aa4fccc240110bd299530503c43f19b8ceacc7f
Clinton wambugu <clintonwambugu@gmail.com>
2022-05-10 00:08:51 +0300

    Add helpful comments

--- run.py
+++ run.py
@@ -4,3 +4,12 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+"""
+Add this to terminal before you start the app
+
+export SECRET_KEY=df84a75c801a375f226ab9fd83b82a
+export SQLALCHEMY_DATABASE_URI='sqlite:///site.db'
+
+"""
